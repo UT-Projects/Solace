1a96085763e214a4d5843d79ef020132cb967255
Ap2626 <adpar2001@gmail.com>
2021-04-02 17:54:13 -0400

    add npm test script

--- unitTests/conftest.py
+++ unitTests/conftest.py
@@ -9,10 +9,8 @@ load_dotenv()
 
 def createuserdatatable():
     try:
-        session = boto3.session.Session(
-            endpoint_url='http://localhost:8000/'
-        )
-        dynamodb = session.resource('dynamodb')
+        session = boto3.session.Session()
+        dynamodb = session.resource('dynamodb', endpoint_url='http://localhost:8000/')
         table = dynamodb.create_table(TableName="user_profile",KeySchema=[{ 'AttributeName': "uuid", 'KeyType': "HASH" }],AttributeDefinitions=[{ 'AttributeName': "uuid", 'AttributeType': "S" }],ProvisionedThroughput={'ReadCapacityUnits': 1,'WriteCapacityUnits': 1} )
         return table.table_status
     except Exception as e:
