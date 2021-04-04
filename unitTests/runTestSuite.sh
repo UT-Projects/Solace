60b05ce2933a49f94e554ade29e5b35cf3a571c1
anshulmodh <35407665+anshulmodh@users.noreply.github.com>
2021-04-03 20:53:51 -0500

    Increase sleep for pytest

--- unitTests/runTestSuite.sh
+++ unitTests/runTestSuite.sh
@@ -3,4 +3,6 @@ rm docker/dynamodb/shared-local-instance.db
 
 pip install -r requirements.txt
 
-docker pull amazon/dynamodb-local && docker-compose up & npm start & (sleep 5 && pytest && docker-compose down && npm stop)
+docker pull amazon/dynamodb-local
+
+ docker-compose up & npm start & (sleep 20 && pytest && docker-compose down && npm stop)
