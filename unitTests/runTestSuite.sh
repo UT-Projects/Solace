f21094d84cee075a5926fc3d5c966f58266511e3
anshulmodh <35407665+anshulmodh@users.noreply.github.com>
2021-04-04 17:20:51 -0500

    Add coverage report generation step

--- unitTests/runTestSuite.sh
+++ unitTests/runTestSuite.sh
@@ -12,3 +12,7 @@ docker pull amazon/dynamodb-local
 echo Iniatliazing...
 
 docker-compose up & nyc node index.js & (sleep 5 && pytest && docker-compose down && npm stop)
+
+echo Formatting coverage report
+
+nyc report --reporter=text-lcov > coverage.lcov
